a80515ad70a8a2ad9423b1ace4b1b4c7376aa41b
skarse <50168777+CPPGhettoIrony@users.noreply.github.com>
2025-06-01 19:53:50 +0200

    Primer commit

--- main.c
+++ main.c
@@ -26,15 +26,16 @@ int main(){
 	
 	//Inicializamos el resto de periféricos: i2c, uart, lcd, etc.
 	glcd_inicializar();
-	
-  
+
 	//Habilitamos las interrupciones globalmente
 	__enable_irq();
 	
 	while(1){
-		__wfi();	//Pasamos al modo Sleep hasta que ocurra una interrupción pero 
-							//siguen funcionando los periféricos (PWM, I2C, etc)
-					
+		//__wfi();	//Pasamos al modo Sleep hasta que ocurra una interrupción pero 
+							  //siguen funcionando los periféricos (PWM, I2C, etc)
+		
+		glcd_xprintf(0, 0, BLANCO, NEGRO, FUENTE8X16, "Test");		
+		
 		//__wfe(); 	//Pasamos al modo DeepSleep hasta que ocurra una interrupción 
                 //NMI, EINT0-3, GPIO, RTC y demás recogidas en el 3.12.2. Se 
                 //detiene la ejecución de todos los periféricos excepto el RTC
@@ -48,15 +49,15 @@ int main(){
  *          b2 TIMER1
  *          b3 UART0
  *          b4 UART1
- *          b7 I2C0
+ *          b7 I2C0		Este periférico será utilizado, así que no se desactivará
  *          b9 RTC
  *          b19 I2C1
  *          b26 I2C2
  */
 void bajo_consumo(void){
-	LPC_SC->PCONP &= (LPC_SC->PCONP & ~((1u<<1)|(1u<<2)|      //TIMER0  TIMER1
-                                      (1u<<3)|(1u<<4)|      //UART0   UART1
-                                      (1u<<7)|(1u<<9)|      //I2C0    RTC
+	LPC_SC->PCONP &= (LPC_SC->PCONP & ~((1u<<1) |(1u<<2)|      //TIMER0  TIMER1
+                                      (1u<<3) |(1u<<4)|      //UART0   UART1
+                            /*(1u<<7)*/        (1u<<9)|      //I2C0    RTC
                                       (1u<<19)|(1u<<26)));  //I2C1    I2C2
 	LPC_SC->PCONP1 = 0;//Comparador
 }
